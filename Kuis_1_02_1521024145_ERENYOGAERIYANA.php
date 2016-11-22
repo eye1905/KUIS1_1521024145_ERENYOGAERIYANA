dd57fef6db3942e2716c27b2ba271e0444436080
cfcyoga@gmail.com <iamchelsea1905>
2016-11-22 19:55:35 +0700

    KUIS
    SATUDUATOGA

---
+++ Kuis_1_02_1521024145_ERENYOGAERIYANA.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>AKN Bojonegoro</title>
+</head>
+<body>
+<?php
+$kalimat = array
+$kalimat = (array("EREN YOGA 1521024145 ERIYANA");
+echo $kalimat[0][0]. .$kalimat[0][1]. .$kalimat[0][2].;
+?>
+</body>
+</html>
